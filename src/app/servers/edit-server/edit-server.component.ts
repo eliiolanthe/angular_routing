a9559f8aaf634e2497dd124a7603946d904f8d0b
Elvira Paulin <eli.paulin@gmail.com>
2018-11-04 23:47:00 +0100

    Working on routes

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -11,10 +12,18 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private activeRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log(this.activeRoute.snapshot.queryParams);
+    console.log(this.activeRoute.snapshot.fragment);
+
+    this.activeRoute.queryParams.subscribe();
+    this.activeRoute.fragment.subscribe();
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
-  constructor() { }
+  constructor(private activeRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.activeRoute.snapshot.params['id'],
+      name: this.activeRoute.snapshot.params['name']
+    };
+
+    // Subscribe to get notified of change in params
+    this.paramsSubscription = this.activeRoute.params
+      .subscribe((params: Params) => {
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+      });
+  }
+
+  // not necessary unless using custom Observables
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
